db9398f0459e2f34c33a7efb6b8d7f9471699e94
Suyash Dabral <s.dabral2001@gmail.com>
2023-04-27 20:29:42 +0530

    Files
    files related to the website

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*active header when window scroll down to 100px */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
